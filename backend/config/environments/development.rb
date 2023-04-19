3ee5f4f11f07c8dee141fe1deab31bb4bfec3f02
cjsmi <ClarenceSmith90@gmail.com>
2023-04-19 17:10:56 -0400

    secure smtp details

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'ClarenceSmith90@gmail.com',
-    password: 'aimkvqormpyjhrbh',
+    user_name:  Rails.application.credentials[:GMAIL_USERNAME],
+    password:  Rails.application.credentials[:GMAIL_PASSWORD],
     authentication: 'plain',
     enable_starttls_auto: true
   }
