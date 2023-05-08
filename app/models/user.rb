8cf427a3b4457472b40cfcb8b9f4736106a857f2
cjsmi <ClarenceSmith90@gmail.com>
2023-05-08 09:34:03 -0400

    annotate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 # Table name: users
 #
 #  id              :bigint           not null, primary key
-#  first_name       :string           not null
-#  last_name        :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
 #  email           :string           not null
 #  is_manager      :boolean          default(FALSE)
 #  password_digest :string           not null
